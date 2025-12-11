ddead2c7cf5983963c388a9c3c459157edb2cd40
ganesh2003 <sakshivpatil211@gmail.com>
2025-12-10 23:22:38 -0800

    java

---
+++ SPRING_BOOT/FullStackWebApp/FullStackProductWebApp/src/main/java/com/demo/FullStackProductWebApp/FullStackProductWebApp.java
@@ -0,0 +1,13 @@
+package com.demo.FullStackProductWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackProductWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackProductWebApp.class, args);
+	}
+
+}
